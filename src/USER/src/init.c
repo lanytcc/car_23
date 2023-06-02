8c54d4629906e5df042a90b21d3f7127a3b0ccf3
maye174 <96584640+maye174@users.noreply.github.com>
2023-06-02 19:47:30 +0800

    new version

--- src/USER/src/init.c
+++ src/USER/src/init.c
@@ -7,20 +7,18 @@ void init() {
     board_init();
     Delay_Init();
 
-    ips114_init();     //��ʼ����Ļ
+    ips114_init();     
     show_message("car_23-Maye", 0);
     show_message("version:0.1.0.2  f=30", 0);
     show_message("init_begin...", 0);
     Delay_Ms(2000);
 
-    /* ��ʼ����� */
     show_message("init_motor...", 0);
     motor_init(left);
     motor_init(right);
     show_message("  ok", strlen("init_motor"));
     Delay_Ms(200);
 
-    /* ��ʼ������ */
     show_message("init_key...", 0);
     key_init(K1);
     key_init(K2);
@@ -37,19 +35,16 @@ void init() {
     show_message("  ok", strlen("init_key"));
     Delay_Ms(200);
 
-    /* ��ʼ������LED */
     show_message("init_led...", 0);
     board_led_init(LED1);
     board_led_init(LED2);
     show_message("  ok", strlen("init_led"));
     Delay_Ms(200);
 
-    /* ��ʼ��RGB���� */
     show_message("init_rgb...", 0);
     ws2812b_init();
     show_message("  ok", strlen("init_rgb"));
     Delay_Ms(200);
-
 
     show_message("init_camera...", 0);
     mt9v03x_init_dvp();
