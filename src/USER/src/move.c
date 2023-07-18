f581d019ca0fe6a9124a624e1a89d6a2294043c8
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-18 21:37:00 +0800

    fix speed_show

--- src/USER/src/move.c
+++ src/USER/src/move.c
@@ -9,9 +9,9 @@ void car_move(int16_t f){
 
     int left_speed = _speed - f * 10;
     int right_speed = _speed + f * 10;
-    
+
     if (speed_show) {
-        sprintf(buf, " %d|%d ", left_speed, left_speed);
+        sprintf(buf, " %d|%d ", left_speed, right_speed);
         show_right_top_message(buf);
     }
 
