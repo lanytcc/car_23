17d6a74eee067233a55f8dddf99cc95fdf201fe2
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-18 23:20:32 +0800

    fix pow

--- src/USER/src/move.c
+++ src/USER/src/move.c
@@ -48,24 +48,24 @@ void cal_speeds(int *left_speed, int *right_speed){
 
     if(diff_x > 0){ 
         // x增大,向右倾斜
-        *right_speed = BASE_SPEED + pow(abs(diff_x), 3);
-        *left_speed = BASE_SPEED - pow(abs(diff_x), 3);
+        *right_speed = BASE_SPEED + pow(abs(diff_x), 3.0);
+        *left_speed = BASE_SPEED - pow(abs(diff_x), 3.0);
     }
     else if(diff_x < 0){
         // x减小,向左倾斜
-        *left_speed = BASE_SPEED - pow(abs(diff_x), 3);
-        *right_speed = BASE_SPEED + pow(abs(diff_x), 3);
+        *left_speed = BASE_SPEED - pow(abs(diff_x), 3.0);
+        *right_speed = BASE_SPEED + pow(abs(diff_x), 3.0);
     }
 
     if(diff_y > 0){
         // y增大
-        *left_speed += pow(abs(diff_y), 2);
-        *right_speed += pow(abs(diff_y), 2); 
+        *left_speed += pow(abs(diff_y), 2.0);
+        *right_speed += pow(abs(diff_y), 2.0); 
     }
     else if(diff_y < 0){
         // y减小
-        *left_speed -= pow(abs(diff_y), 2);
-        *right_speed -= pow(abs(diff_y), 2);
+        *left_speed -= pow(abs(diff_y), 2.0);
+        *right_speed -= pow(abs(diff_y), 2.0);
     }
 
     // 限制速度范围
