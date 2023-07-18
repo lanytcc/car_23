e8bd3deb7e2895c2f2aff72d33f4b22e872618fe
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-18 21:16:57 +0800

    speed up

--- src/USER/src/move.c
+++ src/USER/src/move.c
@@ -1,12 +1,12 @@
 #include "headfile.h"
 #include "move.h"
 
-int _speed = 3000;
+int _speed = 5000;
 
 void car_move(int16_t f){
 
-    int left_speed = _speed - f * 15;
-    int right_speed = _speed + f * 15;
+    int left_speed = _speed - f * 30;
+    int right_speed = _speed + f * 30;
     if(left_speed < 0){
         left_speed = 0;
     }
