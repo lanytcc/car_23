da074b5bdbe7be9046941dc8acc0f7d5cc339614
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-19 18:07:00 +0800

    Adjust obstacle avoidance

--- src/USER/src/move.c
+++ src/USER/src/move.c
@@ -49,8 +49,8 @@ void cal_speeds(int *left_speed, int *right_speed){
         motor_forward(left, 3000);
         motor_forward(right, 4000);
         Delay_Ms(500);
-        motor_forward(left, 3000);
-        motor_forward(right, 5000);
+        motor_forward(left, 2000);
+        motor_forward(right, 5500);
         Delay_Ms(400);
         motor_forward(left, 4000);
         motor_forward(right, 4000);
