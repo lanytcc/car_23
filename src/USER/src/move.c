0c316897b63e8037612d7517e2f222846f159ceb
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-19 20:43:12 +0800

    adjust

--- src/USER/src/move.c
+++ src/USER/src/move.c
@@ -49,7 +49,7 @@ void cal_speeds(int *left_speed, int *right_speed){
         motor_forward(left, 3000);
         motor_forward(right, 4000);
         Delay_Ms(500);
-        motor_forward(left, 2500);
+        motor_forward(left, 3000);
         motor_forward(right, 5000);
         Delay_Ms(480);
         // motor_forward(left, 4000);
