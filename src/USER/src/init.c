dbcb410549f1c559de8b1fc7b64580f6594d1c2c
maye174 <96584640+maye174@users.noreply.github.com>
2023-07-19 19:37:29 +0800

    adjust

--- src/USER/src/init.c
+++ src/USER/src/init.c
@@ -9,7 +9,7 @@ void init() {
 
     ips114_init();     
     show_message("--car_23-Maye--", 0);
-    show_message("version:0.3.8.3", 0);
+    show_message("version:0.3.8.4", 0);
     show_message("init_begin...", 0);
     Delay_Ms(2000);
 
